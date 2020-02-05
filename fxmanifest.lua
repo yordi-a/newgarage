ef7720365475a39fae9806a6b85fb36a4398505e
yordi-a <yordi@outlook.com.tr>
2020-02-05 20:27:23 +0300

    New Garage

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+client_scripts {
+	"config.lua",
+	"client/functions.lua",
+	"client/main.lua"
+}
+
+server_scripts {
+	"@mysql-async/lib/MySQL.lua",
+	"config.lua",
+	"server/main.lua",
+	"server/functions.lua",
+	"server/database.lua"
+}
